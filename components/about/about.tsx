359014604ab8bcfd7f20108b575f2205d53dd98e
roni25536 <72336831+roni25536@users.noreply.github.com>
2022-11-28 15:19:46 +0200

    Update about.tsx

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -4,7 +4,7 @@ const about = () => {
   return (
     <div className={styles.root}>
       <h3 className={styles.firstText}>
-        Hi, My Name Is <h1 className={styles.secondText}>Roni Berlin.</h1>{" "}
+        Hi, My Name Is <h1 className={styles.secondText}>Roni Berlin.</h1>
         <h2 className={styles.thirdText}>I'm DevOps Engineer.</h2>
       </h3>
     </div>
